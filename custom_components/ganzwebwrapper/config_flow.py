b8044197cfe706d8ea50c92ff5b7b345c3751d87
Gabriele Corio <gabriele.corio@gmail.com>
2023-11-27 22:20:27 +0100

    Remove unused library

--- custom_components/ganzwebwrapper/config_flow.py
+++ custom_components/ganzwebwrapper/config_flow.py
@@ -3,9 +3,6 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.helpers import selector
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from .const import DOMAIN, LOGGER
 
